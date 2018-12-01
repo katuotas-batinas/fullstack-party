2508760c2e7b547761acaa5b01181835b571a179
katuotas-batinas <katinas@ember.lt>
2018-12-01 19:22:35 +0200

    Add issue entry component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { IssueEntryComponent } from './components/issue-entry/issue-entry.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'issues', component: IssueListComponent},
+  {path: 'issues/:id', component: IssueEntryComponent},
 ];
 
 @NgModule({
