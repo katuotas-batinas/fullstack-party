f4f4cf966b5a14a30584421602131aebdd9bf0c8
katuotas-batinas <katinas@ember.lt>
2018-12-02 17:24:11 +0200

    Fix routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuestGuard } from './guards/guest.guard';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { IssueListComponent } from './components/issue-list/issue-list.component';
 import { IssueEntryComponent } from './components/issue-entry/issue-entry.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'issues', component: IssueListComponent},
-  {path: 'issues/:id', component: IssueEntryComponent},
+  {path: 'login', component: LoginComponent, canActivate: [GuestGuard]},
+  {path: 'issues', component: IssueListComponent, canActivate: [AuthGuard]},
+  {path: 'issues/:id', component: IssueEntryComponent, canActivate: [AuthGuard]},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
 
 @NgModule({
