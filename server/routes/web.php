db1562a78b888b9d8ec162cb3127d347b7e51bec
katuotas-batinas <katinas@ember.lt>
2018-12-02 03:17:01 +0200

    Add GitHub authorization method

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +15,50 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/config', function (Request $request) use ($router) {
+    $query = http_build_query([
+      'client_id' => env('GITHUB_CLIENT_ID'),
+      'redirect_uri' => url('authorize'),
+      'scope' => 'user',
+    ]);
+
+    $authorizationUrl = 'https://github.com/login/oauth/authorize' . '?' . $query;
+
+    return response()->json([
+      'authorizationUrl' => $authorizationUrl,
+    ]);
+});
+
+$router->get('/authorize', function (Request $request) use ($router) {
+
+  $client = new Client();
+
+  $results = null;
+
+  try {
+    $response = $client->post('https://github.com/login/oauth/access_token', [
+      'json' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'code' => $request->input('code'),
+        'redirect_uri' => url('/'),
+      ]
+    ]);
+
+    parse_str($response->getBody()->getContents(), $results);
+  } catch(RequestException $e) {
+    return redirect('/login');
+  }
+
+  if(!$results || !isset($results['access_token'])) {
+    return redirect('/login');
+  }
+
+  $request->session()->put('access_token', $results['access_token']);
+
+  return redirect('/');
+});
+
+$router->get('{all:.*}', function ($path) use ($router) {
+  return file_get_contents($router->app->basePath('public/client/index.html'));
 });
