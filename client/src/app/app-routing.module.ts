a8970f2516949cd18366bbe1455fdb942b746fb9
Augis <gungaudas@gmail.com>
2018-12-03 15:05:14 +0200

    Add route guards

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { IssueEntryComponent } from './components/issue-entry/issue-entry.compon
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent, canActivate: [GuestGuard]},
-  {path: 'issues', component: IssueListComponent, canActivate: []},
-  {path: 'issues/:owner/:repo/:number', component: IssueEntryComponent, canActivate: []},
+  {path: 'issues', component: IssueListComponent, canActivate: [AuthGuard]},
+  {path: 'issues/:owner/:repo/:number', component: IssueEntryComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
